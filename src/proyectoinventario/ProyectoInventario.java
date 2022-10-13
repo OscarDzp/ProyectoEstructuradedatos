580291795e845b931986adf7f52e5c925c0d90d5
OscarDzp <107152960+OscarDzp@users.noreply.github.com>
2022-10-13 17:24:40 -0600

    1
    1

--- src/proyectoinventario/ProyectoInventario.java
+++ src/proyectoinventario/ProyectoInventario.java
@@ -15,6 +15,12 @@ public class ProyectoInventario {
      */
     public static void main(String[] args) {
    	System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
     }
     
 }
