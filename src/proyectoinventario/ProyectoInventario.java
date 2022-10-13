990a83e6e454e0eae6dc230dbaef8f734e5f420e
OscarDzp <107152960+OscarDzp@users.noreply.github.com>
2022-10-13 14:57:18 -0600

    prueba 2.0
    Prueba de Funcionamiento

--- src/proyectoinventario/ProyectoInventario.java
+++ src/proyectoinventario/ProyectoInventario.java
@@ -14,7 +14,7 @@ public class ProyectoInventario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-   
+   	System.out.println("Hola Mundo");
     }
     
 }
